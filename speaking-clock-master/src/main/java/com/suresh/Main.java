bf5b58805377f2dc542cf252dfa67b2cf979c43c
sureshrajuv0199 <99584305+sureshrajuv0199@users.noreply.github.com>
2022-02-13 14:03:36 +0530

    Update Main.java

--- speaking-clock-master/src/main/java/com/suresh/Main.java
+++ speaking-clock-master/src/main/java/com/suresh/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		BasicSpeakingClock basicSpeakingClock = new BasicSpeakingClock();
 		basicSpeakingClock.convert("11:55");
+		basicSpeakingClock.convert("00:00");
+		basicSpeakingClock.convert("12:00");
 	}
 
 }
